db243d6def9466f44e726821271f58d1164d86bf
ollanga <ollangamoubambadavid@gmail.com>
2019-07-08 00:15:04 +0200

    login form

---
+++ asset/js/script.js
@@ -0,0 +1,60 @@
+// Début scroll
+
+var slideLeft = {
+    distance: '150%',
+	origin: 'left',
+	duration: 1500,
+	
+	
+	};
+
+	var slideRight = {
+		distance: '150%',
+		origin: 'right',
+		duration: 1500,
+		
+    };
+    
+    var slideBottom = {
+		distance: '150%',
+		origin: 'bottom',
+		duration: 1000,
+		
+	};
+
+    var slideBottom2 = {
+		distance: '150%',
+		origin: 'bottom',
+		duration: 1500,
+		
+	};
+
+
+    var slidetop = {
+		distance: '150%',
+		origin: 'top',
+		duration: 1000,
+		
+	};
+
+
+    var slidetop2 = {
+		distance: '150%',
+		origin: 'top',
+		duration: 1500,
+		
+	};
+
+
+	ScrollReveal().reveal('.left1', slideLeft);
+    ScrollReveal().reveal('.right1', slideRight);
+    ScrollReveal().reveal('#bas', slideBottom);
+    ScrollReveal().reveal('.bas1', slideBottom2);
+    ScrollReveal().reveal('.top', slidetop);
+    ScrollReveal().reveal('.top2', slidetop2);
+    
+    // Fin scroll
+
+// email
+
+
